84a232e96bc9e44a45b4c0080fa651046c64dc68
d-alejo90 <diecel1990@gmail.com>
2025-09-10 21:27:30 -0500

    fix(webhook): Update CreateManualOrder webhook script
    - Switch to test order data from file for local testing
    - Add log file for tracking webhook execution
    - Comment out alternative store configuration

--- public/WebHooks/CreateManualOrder.php
+++ public/WebHooks/CreateManualOrder.php
@@ -9,6 +9,6 @@ require_once __DIR__ . '/../../src/Webhooks/CreateManualOrder.php';
 // $webhookData = file_get_contents('php://input');
 $webhookData = file_get_contents(__DIR__ . '/test_order.json');
 $logFile = "create_manual_order_wh_run.txt";
-// $webhook = new CreateManualOrderWebhook($webhookData, $logFile, "friko-ecommerce.myshopify.com", "orders/create");
-$webhook = new CreateManualOrderWebhook($webhookData, $logFile, "mizooco.myshopify.com", "orders/create");
+$webhook = new CreateManualOrderWebhook($webhookData, $logFile, "friko-ecommerce.myshopify.com", "orders/create");
+// $webhook = new CreateManualOrderWebhook($webhookData, $logFile, "mizooco.myshopify.com", "orders/create");
 $webhook->handleWebhook();
